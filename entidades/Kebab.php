6d6310ea79b3e87d027c52df593aed4295c454f8
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-10-27 18:05:51 +0100

    Creada la estructura de la clase Kebab

---
+++ entidades/Kebab.php
@@ -0,0 +1,35 @@
+<?php
+class Kebab {
+    private $id;
+    private $nombre;
+    private $precioBase;
+
+    public function __construct($nombre, $precioBase, $id = null) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->precioBase = $precioBase;
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getPrecioBase() {
+        return $this->precioBase;
+    }
+
+    // Setters
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setPrecioBase($precioBase) {
+        $this->precioBase = $precioBase;
+    }
+}
+?>
