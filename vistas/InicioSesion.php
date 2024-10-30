fb368a260386fc4a56c0d98f782af94f61402c38
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-10-30 19:37:53 +0100

    Se ha agregado una función para buscar por email

--- vistas/InicioSesion.php
+++ vistas/InicioSesion.php
@@ -1,5 +1,7 @@
-<?php
 
+<form action="./controladores/procesarInicioSesion.php" method="POST">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Contraseña" required>
+    <button type="submit">Iniciar sesión</button>
+</form>
 
-
-?>
