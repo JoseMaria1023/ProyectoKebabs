1fefb1fe57c2a7bb33912c6902ccb52aff7acf27
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-10-30 20:03:14 +0100

    Se ha cambiado el nombre de la carpeta

---
+++ vistas_generales/InicioSesion.php
@@ -0,0 +1,7 @@
+
+<form action="./controladores/procesarInicioSesion.php" method="POST">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Contraseña" required>
+    <button type="submit">Iniciar sesión</button>
+</form>
+
