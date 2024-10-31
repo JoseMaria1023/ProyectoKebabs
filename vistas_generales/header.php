7e40ef7b602755d00cffe803669614743a1da1f6
JoseMaria1023 <josemariaangulorivas1023@gmail.com>
2024-10-31 08:33:16 +0100

    cambios menores

--- vistas_generales/header.php
+++ vistas_generales/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda Online de Kebabs</title>
-    <link rel="stylesheet" href="./css/estilo.css">
+    <link rel="stylesheet" href="./css/header.css">
 <body>
     <header>
         <h1>Tienda Online de Kebabs</h1>
