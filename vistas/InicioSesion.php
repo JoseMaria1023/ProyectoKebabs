3a11787f3fa61ac4a0aa78ff101d264f9bfb88af
JoseMaria1023 <josemariaangulorivas1023@gmail.com>
2024-10-31 09:53:31 +0100

    Eliminar carpeta

--- vistas/InicioSesion.php
+++
@@ -1,7 +0,0 @@
-
-<form action="./controladores/procesarInicioSesion.php" method="POST">
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Contraseña" required>
-    <button type="submit">Iniciar sesión</button>
-</form>
-
--- vistas/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- footer.php -->
-<footer>
-        <p>&copy; <?php ?> Tienda venta de Kebabs</p>
-    </footer>
-</body>
-</html>
