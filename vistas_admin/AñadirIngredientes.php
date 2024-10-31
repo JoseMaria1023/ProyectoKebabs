d3adea0e9353b89840ea33ebf30c56ae2a36068d
JoseMaria1023 <josemariaangulorivas1023@gmail.com>
2024-10-31 09:30:58 +0100

    cambios menores

--- vistas_admin/AñadirIngredientes.php
+++ vistas_admin/AñadirIngredientes.php
@@ -1,21 +1,33 @@
-<div class="container">
-    <h1>Gestión de Ingredientes</h1>
-    <form action="procesarIngrediente.php" method="POST">
-        <label for="nombre">Nombre del Ingrediente:</label>
-        <input type="text" id="nombre" name="nombre" required>
-
-        <label for="descripcion">Descripción:</label>
-        <textarea id="descripcion" name="descripcion" required></textarea>
-
-        <label for="precio">Precio:</label>
-        <input type="number" id="precio" name="precio" step="0.01" required>
-
-        <label for="alergenos">Alérgenos:</label>
-        <input type="text" id="alergenos" name="alergenos" placeholder="Ej. Gluten, Lactosa">
-
-        <label for="foto">Foto del Ingrediente:</label>
-        <input type="file" id="foto" name="foto" accept="image/*" required>
-
-        <button type="submit">Añadir Ingrediente</button>
-    </form>
+<link rel="stylesheet" href="./css/añadiringredientes.css">
+<div class="gestion-ingredientes">
+    <div class="container">
+        <h1>Gestionar Ingredientes</h1>
+        <form>
+            <div class="foto">
+                <label for="foto">Foto del Ingrediente:</label>
+                <input type="file" id="foto" name="foto" required>
+            </div>
+            <div class="infomarcion-container">
+                <div>
+                    <label for="name">Nombre del Ingrediente:</label>
+                    <input type="text" id="name" name="name" required>
+                </div>
+                <div>
+                    <label for="description">Descripción:</label>
+                    <textarea id="description" name="description" required></textarea>
+                </div>
+                <div class="additional-info">
+                    <div>
+                        <label for="alergenos">Alergenos:</label>
+                        <input type="text" id="alergenos" name="alergenos">
+                    </div>
+                    <div>
+                        <label for="precio">Precio:</label>
+                        <input type="number" id="precio" name="precio" required>
+                    </div>
+                </div>
+            </div>
+            <button type="submit">Agregar Ingrediente</button>
+        </form>
+    </div>
 </div>
