c3704fcaf791b2d852232cbef67132d2c581abe2
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-11-04 21:55:11 +0100

    Cambios menores

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- index.php -->
-<?php include './vistas_generales/header.php'; ?>
+<?php include './vistas_usuarios/headerLogueado.php'; ?>
 
-<?php include './vistas_generales/registro.php'; ?>
+<?php include './vistas_admin/crearKebab.php'; ?>
 
 <?php include './vistas_generales/footer.php'; ?>
