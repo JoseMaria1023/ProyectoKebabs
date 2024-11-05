1ee39631f53f323dbdbf0a697d626a2af26f8ade
JoseMaria1023 <josemariaangulorivas1023@gmail.com>
2024-11-05 13:55:32 +0100

    cambios menores

--- Metodos/FuncionLogin.php
+++ Metodos/FuncionLogin.php
@@ -1,33 +1,37 @@
 <?php
 
-function iniciaSesion() {
-    session_start();
-}
+include_once '../cargadores/autocargadores.php';
 
-function cierraSesion() {
-    session_destroy(); 
-}
 
-function entralogin($user) {
-    iniciaSesion();
-    if (!empty($user)) {
-        $_SESSION['user'] = $user;
+class FuncionLogin {
+    public static function iniciaSesion() {
+        session_start();
+    }
+
+    public static function cierraSesion() {
+        session_destroy(); 
+    }
+
+    public static function entralogin($usuario) {
+        self::iniciaSesion();
+        if (!empty($usuario)) {
+            $_SESSION['user'] = $usuario;
+        }
+    }
+
+    public static function estalogeado() {
+        self::iniciaSesion();
+        return isset($_SESSION['user']);
+    }
+
+    public static function leer_sesion() {
+        self::iniciaSesion();
+        return isset($_SESSION['user']) ? $_SESSION['user'] : null;
+    }
+
+    public static function escribir_sesion($key, $value) {
+        self::iniciaSesion();
+        $_SESSION[$key] = $value;
     }
 }
-
-function estalogeado() {
-    iniciaSesion();
-    return isset($_SESSION['user']);
-}
-
-function leer_sesion() {
-    iniciaSesion();
-    return isset($_SESSION['user']) ? $_SESSION['user'] : null;
-}
-
-function escribir_sesion($key, $value) {
-    iniciaSesion();
-    $_SESSION[$key] = $value;
-}
-
 ?>
