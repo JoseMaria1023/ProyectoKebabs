66e36b53c427442c4106d2b4511f049d07e813cf
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-11-05 19:57:17 +0100

    Nuevos repositorios y cambios menores

--- repositorios/RepoLineaPedido.php
+++ repositorios/RepoLineaPedido.php
@@ -3,7 +3,7 @@
 include_once '../cargadores/autocargadores.php';
 
 
-class RepoKebabIngredientes{
+class RepoIngredientesAlergenos{
     private $conexion;
 
     public function __construct() {
@@ -11,8 +11,8 @@ class RepoKebabIngredientes{
     }
 
   
-    public function guardarKebabIngredientes(Ingredientes $ingredientes) {
-        $stmt = $this->conexion->prepare("INSERT INTO kebab_ingredientes (nombre, descripcion, precio, alergeno) VALUES (?, ?, ?, ?)");
+    public function guardarIngredientesAlergenos(Ingredientes $ingredientes) {
+        $stmt = $this->conexion->prepare("INSERT INTO ingrediente_alergenos (nombre, descripcion, precio, alergeno) VALUES (?, ?, ?, ?)");
         $stmt->execute([
             $ingredientes->getNombre(), 
             $ingredientes->getDescripcion(), 
@@ -23,8 +23,8 @@ class RepoKebabIngredientes{
         return $ingredientes;
     }
 
-    public function actualizarKebabIngredientes(Ingredientes $ingredientes) {
-        $stmt = $this->conexion->prepare("UPDATE kebab_ingredientes SET nombre = ?, email = ?, direccion = ?, rol = ? WHERE id = ?");
+    public function actualizarIngredientesAlergenos(Ingredientes $ingredientes) {
+        $stmt = $this->conexion->prepare("UPDATE ingrediente_alergenos SET nombre = ?, email = ?, direccion = ?, rol = ? WHERE id = ?");
         return $stmt->execute([
             $usuario->getNombre(), 
             $usuario->getEmail(), 
@@ -33,10 +33,11 @@ class RepoKebabIngredientes{
         ]);
     }
 
-    public function eliminarKebabIngredientes($id) {
-        $stmt = $this->conexion->prepare("DELETE FROM kebab_ingredientes WHERE id = ?");
+    public function eliminarIngredientesAlergenos($id) {
+        $stmt = $this->conexion->prepare("DELETE FROM ingrediente_alergenos WHERE id = ?");
         return $stmt->execute([$id]);
     }
 
 
 }
+?>
