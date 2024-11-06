2cd20ce8ea2c11fb11f47a0a1d1536a64bc39c1f
JoseMaria1023 <josemariaangulorivas1023@gmail.com>
2024-11-06 14:07:13 +0100

    Nuevo controlador para los kebabs y se ha modificado para poder insertar las fotos en base 64

--- controladores/ProcesarKebab.php
+++ controladores/ProcesarKebab.php
@@ -6,10 +6,8 @@ $directorio = '../imagenes/';
 
 
 $nombre = $_POST['nombre'];
-$descripcion = $_POST['descripcion'];
 $precio = $_POST['precio'];
-$alergeno = $_POST['alergenos'];
-
+$descripcion = $_POST['descripcion'];
 $foto = null;
 
 $Archivo = $_FILES['foto']['name'];
@@ -20,12 +18,5 @@ if (move_uploaded_file($_FILES['foto']['tmp_name'], $rutaArchivo)) {
     $contenidoBinario = file_get_contents($rutaArchivo);
     $foto = base64_encode($contenidoBinario);
 }
-$ingredientes = new Ingredientes($nombre,$descripcion, $precio, $alergeno, $foto);
 
-$repoIngredientes = new RepoIngredientes();
-$GuardarIngrediente=$repoIngredientes->guardarIngredientes($ingredientes);
-
-if ($GuardarIngrediente) {
-    echo "Nuevo Ingrediente registrado.";
-} 
 ?>
--- controladores/procesarRegistro.php
+++ controladores/procesarRegistro.php
@@ -1,12 +1,25 @@
 <?php
 include_once '../cargadores/autocargadores.php';
 
+$directorio = '../imagenes/';
+
+
 $username = $_POST['username'];
 $password = $_POST['contrasenia'];
 $email = $_POST['email'];
 $direccion = $_POST['direccion'];
 
-$usuario = new Usuarios($username, $password, $email, $direccion, "usuario");
+$foto = null;
+
+$Archivo = $_FILES['foto']['name'];
+
+$rutaArchivo = $directorio . $Archivo;
+
+if (move_uploaded_file($_FILES['foto']['tmp_name'], $rutaArchivo)) {
+    $contenidoBinario = file_get_contents($rutaArchivo);
+    $foto = base64_encode($contenidoBinario);
+}
+$usuario = new Usuarios($username, $password, $email, $direccion, "usuario", $foto);
 
 $repoUsuarios = new RepoUsuarios();
 $usuarioGuardado = $repoUsuarios->guardar($usuario);
