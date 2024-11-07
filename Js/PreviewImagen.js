94c9aa839f8e8c8bd357beb08b4c17b8fc73e91c
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-11-07 21:46:16 +0100

    Previsualizar imagen

---
+++ Js/PreviewImagen.js
@@ -0,0 +1,19 @@
+function previewImage(event) {
+    var preview = document.getElementById('preview');
+    var file = event.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function() {
+        preview.src = reader.result;
+        preview.style.display = 'block';
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var fileInput = document.getElementById('foto');
+    fileInput.addEventListener('change', previewImage);
+});
