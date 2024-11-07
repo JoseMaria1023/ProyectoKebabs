593f9e895a53a0c9107a17cdfaf4c14b845341c5
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-11-07 21:44:51 +0100

    Cambios menores

--- vistas_admin/AñadirIngredientes.php
+++ vistas_admin/AñadirIngredientes.php
@@ -3,14 +3,18 @@ include_once '../cargadores/autocargadores.php';
 cargarCSS('headerLogueado', 'añadiringredientes', 'footer');
 ?>
 <?php include '../vistas_usuarios/HeaderLogueado.php'; ?>
+<script src="../Js/PreviewImagen.js"></script>
 <div class="gestion-ingredientes">
     <div class="container">
         <h1>Gestionar Ingredientes</h1>
         <form action="../controladores/ProcesarIngredientes.php" method="POST" enctype="multipart/form-data">
-            <div class="foto">
-                <label for="foto">Foto del Ingrediente:</label>
-                <input type="file" name="foto" id="foto">
-                </div>
+        <div class="foto">
+        <label for="foto">Seleccionar Imagen</label>
+        <input type="file" name="foto" id="foto" />
+        <div id="preview-container" class="image-preview">
+            <img id="preview" src="">
+        </div>
+    </div>
             <div class="infomarcion-container">
                 <div>
                     <label for="nombre">Nombre del Ingrediente:</label>
