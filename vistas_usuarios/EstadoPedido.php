fc7acfb0344556e591d28ad4988ddca191d4c94d
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-11-07 21:45:21 +0100

    Nueva vista para el estado del pedido

---
+++ vistas_usuarios/EstadoPedido.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="./css/estadoPedido.css">
+<main class="contenido-pedido">
+        <section class="estado-pedido">
+            <h2>Estado de tu Pedido</h2>
+            <p class="estado">Estado actual: <span id="estado-actual"></span></p>
+        </section>
+
+        <section class="detalle-pedido">
+            <h2>Detalle de tu Pedido</h2>
+            <div class="items">
+                <div class="item">
+                    <h3></h3>
+                    <p></p>
+                    <button class="mostrar-ingredientes">Ver Ingredientes</button>
+                    <div class="ingredientes">
+                        <ul>
+                           
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
