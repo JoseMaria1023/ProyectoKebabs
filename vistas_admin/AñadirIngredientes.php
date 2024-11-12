11b3421abfe1a9695d805a0cc74d45db115c7b98
josemariaangulorivas1023@gmail.com <josemariaangulorivas1023@gmail.com>
2024-11-12 22:20:14 +0100

    Cambios menores

--- vistas_admin/AñadirIngredientes.php
+++ vistas_admin/AñadirIngredientes.php
@@ -7,41 +7,41 @@ cargarCSS('headerLogueado', 'añadiringredientes', 'footer');
 <div class="gestion-ingredientes">
     <div class="container">
         <h1>Gestionar Ingredientes</h1>
-        <form action="../controladores/ProcesarIngredientes.php" method="POST" enctype="multipart/form-data">
+        <form action="../APIS/ApiIngredientes.php" method="POST" enctype="multipart/form-data">
         <div class="foto">
-
-        <label for="foto">Seleccionar Imagen</label>
-        <input type="file" name="foto" id="foto" />
-        
-        <div id="preview-container" class="image-preview">
-            <img id="preview" src="">
+            <label for="foto">Seleccionar Imagen</label>
+            <input type="file" name="foto" id="foto" />
+            <div id="preview-container" class="image-preview">
+                <img id="preview" src="">
+            </div>
         </div>
-    </div>
-            <div class="infomarcion-container">
+
+        <div class="infomarcion-container">
+            <div>
+                <label for="nombre">Nombre del Ingrediente:</label>
+                <input type="text" name="nombre" required>
+            </div>
+            <div>
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion" name="descripcion" required></textarea>
+            </div>
+            <div class="infoadicional">
                 <div>
-                    <label for="nombre">Nombre del Ingrediente:</label>
-                    <input type="text" name="nombre" required>
+                    <label for="precio">Precio:</label>
+                    <input type="number" id="precio" name="precio" step="0.01" min="0" required>
                 </div>
                 <div>
-                    <label for="descripcion">Descripción:</label>
-                    <textarea id="descripcion" name="descripcion" required></textarea>
-                </div>
-                <div class="infoadicional">
-                    <div>
-                        <label for="precio">Precio:</label>
-                        <input type="number" id="precio" name="precio" step="0.01" min="0" required>
-                    </div>
-                    <div>
-                        <label for="alergenos">Alergenos:</label>
-                        <input type="text" id="alergenos" name="alergenos">
-                    </div>
+                    <label for="alergenos">Alergenos:</label>
+                    <select id="alergenos" name="alergenosArray[]" multiple required>
+                    </select>
                 </div>
             </div>
-            <button type="submit">Añadir Ingrediente</button>
+        </div>
+        <button type="submit">Añadir Ingrediente</button>
         </form>
     </div>
 </div>
+<script src="../Js/MostrarAlergenos.js"></script>
+
 <?php include '../vistas_generales/footer.php'; ?>
-
-
 
