b135180a663617b9716c235f4184be9d3889f663
JoseMaria1023 <josemariaangulorivas1023@gmail.com>
2024-11-25 10:52:27 +0100

    Nueva vista principal del usuario

---
+++ vistas_usuarios/InicioUsuario.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>¡Bienvenido a Kebab Paradise!</title>
+    <link rel="stylesheet" href="./css/InicioUsuario.css">
+</head>
+<body>
+    <div class="bienvenida-container">
+        <div class="bienvenida-content">
+            <h1>¡Bienvenido a Kebab Yi!</h1>
+            <p>Disfruta de los mejores kebabs y los ingredientes más frescos.</p>
+            <a href="./vistas_usuarios/MostrarKebab.php"><button class="boton-comenzar">Explora el Menú</button></a>
+        </div>
+    </div>
+</body>
+</html>
