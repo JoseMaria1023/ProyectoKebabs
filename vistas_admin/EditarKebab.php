65c171140ce10c79d98f4e0f46de0c0c7516fb07
JoseMaria1023 <josemariaangulorivas1023@gmail.com>
2024-11-25 10:53:02 +0100

    Cambios menores

--- vistas_admin/EditarKebab.php
+++ vistas_admin/EditarKebab.php
@@ -13,22 +13,23 @@ cargarCSS('HeaderAdmin', 'EditarKebab', 'footer');
 <body>
     <h1>Editar Kebab</h1>
     
-    <form id="form-editar-Kebab">
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" value="" required>
-        <br>
+    <form id="form-editar-kebab">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" value="">
+    <br>
 
-        <label for="descripcion">Descripcion:</label>
-        <input type="text" id="descripcion" name="descripcion" value="" required>
-        <br>
+    <label for="descripcion">Descripcion:</label>
+    <input type="text" id="descripcion" name="descripcion" value="">
+    <br>
 
-        <label for="Precio">Precio:</label>
-        <input type="decimal" id="precio" name="precio" value="" required>
-        <br>
-        <input type="hidden" id="id-kebab" name="id_kebab" value="">
+    <label for="precio_base">Precio:</label>
+    <input type="number" id="precio_base" name="precio_base" value="" step="0.01">
+    <br>
 
-        <button type="submit">Guardar Cambios</button>
-    </form>
+    <input type="hidden" id="id-kebab" name="id-kebab" value="">
+
+    <button type="submit">Guardar Cambios</button>
+</form>
 
     <script src="../Js/EditarKebab.js"></script>
 </body>
